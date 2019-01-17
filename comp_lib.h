b549a40cef59365a645b998a0155d19f287b3100
Team0468 <46728644+Team0468@users.noreply.github.com>
2019-01-17 16:08:33 -0600

    Add files via upload

--- comp_lib.h
+++ comp_lib.h
@@ -3,14 +3,109 @@
 #define target_theta_45 225000 //numbers for wallaby-4188
 #define target_theta_90 507500
 #define target_theta_180 1061500
-#define target_theta_360 2175000
-#define target_theta_m45 225000
-#define target_theta_m90 507500
-#define target_theta_m180 1061500
-#define target_theta_m360 2175000
-void move(int lsp,int rsp) {
-    mav(right_motor,rsp);
-    mav(left_motor,lsp);
+int target_theta_m45 = target_theta_45;
+int target_theta_m90 = target_theta_90;
+int target_theta_m180 = target_theta_180;
+
+#define analog_white 300
+#define analog_black 3500
+#define digital_right 0
+#define digital_left 1
+#define left_IR 1
+#define right_IR 0
+#define stop 0
+
+
+void move(l_speed,r_speed){
+    mav(right_motor,r_speed);
+    mav(left_motor,l_speed);
+}
+
+int white = 1;
+int black = 2;
+int physical = 3;
+int black_speed;
+void square_up(int ending,int speed){
+    if(speed > 0 && speed < 600){
+        black_speed = 0.5*speed;
+    }
+    else{black_speed = 0.25*speed;}
+    if(ending == 1 || ending == 2){
+        while(1){
+            if(analog(left_IR)<analog_white && analog(right_IR)<analog_white){
+                move(speed,speed);
+            }
+            if(analog(right_IR)>analog_white){
+                move(speed,stop);
+            }
+            if(analog(left_IR)>analog_white){
+                move(stop,speed);
+            }
+            if(analog(left_IR)>analog_white && analog(right_IR)>analog_white) {
+                move(stop,stop);
+                break;
+            }
+        }
+    }
+    if(ending == 3){
+        ao();
+    }
+    switch(ending){
+        case 3:
+            {
+                while(1){
+                    if(digital(digital_right)==0 && digital(digital_left)==0){
+                        move(speed,speed);
+                    }
+                    if(digital(digital_right)==1){
+                        move(speed,stop);
+                    }
+                    if(digital(digital_left)==1){
+                        move(stop,speed);
+                    }
+                    if(digital(digital_right)==1 && digital(digital_left)==1){
+                        move(stop,stop);
+                        break;
+                    }
+                }
+            }
+        case 1:
+            {
+                while(1){
+                    if(analog(left_IR)>analog_white && analog(right_IR)>analog_white){
+                        move(black_speed,black_speed);
+                    }
+                    if(analog(left_IR)<analog_white){
+                        move(stop,black_speed);
+                    }
+                    if(analog(right_IR)<analog_white){
+                        move(black_speed,stop);
+                    }
+                    if(analog(left_IR)<analog_white && analog(right_IR)<analog_white){
+                        move(stop,stop);
+                        break;
+                    }
+                }
+            }
+        case 2:
+            {
+                while(1){
+                    if(analog(left_IR)>analog_white && analog(right_IR)>analog_white){
+                        move(-1*black_speed,-1*black_speed);
+                    }
+                    if(analog(left_IR)<analog_white){
+                        move(stop,-1*black_speed);
+                    }
+                    if(analog(right_IR)<analog_white){
+                        move(-1*black_speed,stop);
+                    }
+                    if(analog(left_IR)<analog_white && analog(right_IR)<analog_white){
+                        move(stop,stop);
+                        break;
+                    }
+                }
+            }
+    }
 }
 
 double bias = 0.0;
@@ -85,10 +180,6 @@ void turn_with_gyro_create(int speed, int deg){
             targetTheta = target_theta_180;
            create_drive_direct(speed,speed*-1);
             break;
-        case 360:
-            targetTheta = target_theta_360;
-            create_drive_direct(speed,speed*-1);
-            break;
         case -45:
             targetTheta = target_theta_m45;
             create_drive_direct(speed*-1,speed);
@@ -99,10 +190,6 @@ void turn_with_gyro_create(int speed, int deg){
             break;
         case -180:
             targetTheta = target_theta_m180;
-            create_drive_direct(speed*-1,speed);
-            break;
-        case -360:
-            targetTheta = target_theta_m360;
             create_drive_direct(speed*-1,speed);
             break;
         default:
@@ -134,10 +221,6 @@ void turn_with_gyro(int speed, int deg){
             targetTheta = target_theta_180;
             move(speed,speed*-1);
             break;
-        case 360:
-            targetTheta = target_theta_360;
-            move(speed,speed*-1);
-            break;
         case -45:
             targetTheta = target_theta_m45;
             move(speed*-1,speed);
@@ -148,10 +231,6 @@ void turn_with_gyro(int speed, int deg){
             break;
         case -180:
             targetTheta = target_theta_m180;
-            move(speed*-1,speed);
-            break;
-        case -360:
-            targetTheta = target_theta_m360;
             move(speed*-1,speed);
             break;
         default:
@@ -166,4 +245,21 @@ void turn_with_gyro(int speed, int deg){
     mav(right_motor, 0);
     mav(right_motor, 0);
 }
+
+void PID_gyro_drive_create(int speed, double time){
+ 	calibrate_gyro();
+    double startTime = seconds();
+    double theta = 0;
+    while ((seconds() - startTime) < time){
+        if (speed > 0){
+            create_drive_direct((speed + (speed * theta/100000)),(speed - (speed * theta/100000)));
+        }
+        else{
+            create_drive_direct((speed - (speed * theta/100000)), (speed + (speed * theta/100000)));
+        }
+        msleep(15);
+        theta += (gyro_z() - bias) * 10;
+    }
+}
+
 
