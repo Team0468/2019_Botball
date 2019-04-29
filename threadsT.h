169056ebcad912d33cbf796c8ad7c140c568d714
Team0468 <46728644+Team0468@users.noreply.github.com>
2019-04-29 15:46:40 -0500

    Delete threadsT.h

--- threadsT.h
+++
@@ -1,7 +0,0 @@
-#include <kipr/botball.h>
-#include </home/root/Documents/KISS/Default User/tournament_file_Bot_A/include/comp_lib.h>
-#include </home/root/Documents/KISS/Default User/tournament_file_Bot_A/include/L_Mechanism.h>
-
-int slow_arm_thread(int x, int speed){
-    	slow_arm(x,speed);
-}
